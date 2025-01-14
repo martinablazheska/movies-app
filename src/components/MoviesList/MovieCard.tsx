3cef3d2b88b4ca20b43e4426799612abce3d296a
Martina Blazheska <blazheska.martina@gmail.com>
2025-01-14 14:11:58 +0100

    feature: Movie card play button
    - Added a play icon to point out that the image is a button
    - Additional style changes for smooth opacity transition

--- src/components/MoviesList/MovieCard.tsx
+++ src/components/MoviesList/MovieCard.tsx
@@ -1,15 +1,19 @@
 import moviePlaceHolder from "@/assets/movie-placeholder.jpg";
 import MovieCardTitle from "@/components/MoviesList/MovieCardTitle";
 import MovieCardDescription from "./MovieCardDescription";
+import Play from "@/icons/Play";
 
 const MovieCard: React.FC<{ title: string }> = ({ title }) => {
   return (
     <div className="w-full h-full display flex flex-col gap-3 p-4 bg-white rounded-lg shadow-md">
-      <img
-        src={moviePlaceHolder}
-        alt="Abstract art placeholder"
-        className="opacity-65 rounded-lg"
-      />
+      <button className="group relative">
+        <img
+          src={moviePlaceHolder}
+          alt="Abstract art placeholder"
+          className="opacity-75 group-hover:opacity-55 rounded-lg group-focus:opacity-75 transition-opacity duration-300 ease-out"
+        />
+        <Play className="absolute inset-0 m-auto h-10 w-10 opacity-0 group-hover:opacity-100 text-white transition-opacity duration-300 ease-out" />
+      </button>
       <div>
         <MovieCardTitle title={title} />
         <MovieCardDescription
