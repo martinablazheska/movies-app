143df33aee4aa99fd64a49cf0a1e9ac2479dfba9
Martina Blazheska <blazheska.martina@gmail.com>
2025-01-15 17:15:08 +0100

    fix: useUrlParams
    - Remove trailing slash from end of pathname
    - Remove unnecessary useCallback

--- src/hooks/useUrlParams.ts
+++ src/hooks/useUrlParams.ts
@@ -1,21 +1,21 @@
-import { useCallback } from "react";
-
 export const useUrlParams = () => {
   const searchParams = {
     page: Number(new URLSearchParams(window.location.search).get("page")) || 1,
     query: new URLSearchParams(window.location.search).get("query") || "",
   };
 
-  const updateUrl = useCallback((page: number, query: string) => {
+  const updateUrl = (page: number, query: string) => {
     const params = new URLSearchParams();
     if (page !== 1) params.set("page", page.toString());
     if (query) params.set("query", query);
 
-    const newUrl = `${window.location.pathname}${
+    const pathname = window.location.pathname.replace(/\/$/, "");
+
+    const newUrl = `${pathname}${
       params.toString() ? `?${params.toString()}` : ""
     }`;
     window.history.pushState({}, "", newUrl);
-  }, []);
+  };
 
   return { searchParams, updateUrl };
 };
